77908d0b71b861d12e923c18cab0ac5b39676809
heybhuvan19 <yoursbhuvan19@gmail.com>
2025-06-26 18:27:56 +0530

    Leap year checker

---
+++ leapyear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int year;
+    printf("Enter the year: ");
+    scanf("%d",&year);
+
+    if(year%4==0){
+        if(year%100!=0){
+            if(year%400==0){
+                printf("It's a leap year");
+            }
+            else{
+                printf("It's not a leap year");
+            }
+        }
+        else{
+            printf("It's a leap year");
+        }
+    }
+    else{
+        printf("It's not a leap year");
+    }
+    
+    return 0;
+}
