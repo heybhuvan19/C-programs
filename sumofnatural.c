e9bec84a2733de039a115a566a19984d02c7526c
heybhuvan19 <yoursbhuvan19@gmail.com>
2025-07-02 18:32:17 +0530

    Updated notes and sum of natural number program

--- sumofnatural.c
+++ sumofnatural.c
@@ -1,3 +1,4 @@
+//using while loop
 #include <stdio.h>
 
 int main(){
@@ -8,6 +9,35 @@ int main(){
         sum = sum + i;
         i++;
     }
+    printf("The sum of first %d natural number is %d",number,sum);
+    return 0;
+}
+
+//using while for loop
+#include <stdio.h>
+
+int main(){
+    int number,sum=0,i;
+    printf("Enter the number:");
+    scanf("%d",&number);
+    for(i=0; i<=number; i++){
+        sum = sum + i;
+    }
+    printf("The sum of first %d natural number is %d",number,sum);
+    return 0;
+}
+ 
+//using do-while loop
+#include <stdio.h>
+
+int main(){
+    int number,i=0, sum=0;
+    printf("Enter the number:");
+    scanf("%d",&number);
+    do{
+        sum = sum + i;
+        i++;
+    }while(i<=number);
     printf("The sum of first %d natural number is %d",number,sum);
     return 0;
 }
